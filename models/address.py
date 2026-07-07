f08e8ee7efe291d4b99c0f2448864cc2d72a020d
Arlette Diaz <adiaz@catalyte.io>
2026-07-07 11:34:36 -0500

    created address class with attributes street, city, state, and zip_code

---
+++ models/address.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+''' Address class to represent a physical address of where patient is located.'''
+
+@dataclass
+class Address:
+    '''Attributes:
+    street (str): The street address of the patient.
+    city (str): The city where the patient resides.
+    state (str): The state where the patient resides.
+    zip_code (str): The ZIP code of the patient's address.
+    '''
+    street: str
+    city: str
+    state: str
+    zip_code: str
