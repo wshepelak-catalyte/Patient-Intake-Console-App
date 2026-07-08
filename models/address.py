113ca39129030afab4c9658f8285388a6aecbd57
wshepelak-catalyte <wshepelak@catalyte.io>
2026-07-08 10:33:34 -0500

    fixed linter errors for address.py and gender.py

--- models/address.py
+++ models/address.py
@@ -1,15 +1,21 @@
+"""
+Defines an address class used to group location data together into one object.
+"""
 from dataclasses import dataclass
 
-''' Address class to represent a physical address of where patient is located.'''
 
 @dataclass
 class Address:
-    '''Attributes:
-    street (str): The street address of the patient.
-    city (str): The city where the patient resides.
-    state (str): The state where the patient resides.
-    zip_code (str): The ZIP code of the patient's address.
-    '''
+    """
+    Address class to represent a physical address of where patient is located.    
+
+    Attributes:
+        street (str): The street address of the patient.
+        city (str): The city where the patient resides.
+        state (str): The state where the patient resides.
+        zip_code (str): The ZIP code of the patient's address.
+    """
+
     street: str
     city: str
     state: str
--- models/gender.py
+++ models/gender.py
@@ -1,8 +1,12 @@
+"""
+Defines the Gender enumeration used to represent patient gender values.
+"""
+
 from enum import Enum
 
 class Gender(Enum):
     """Enumeration representing patient gender options."""
-    
+
     MALE = "Male"
     FEMALE = "Female"
     OTHER = "Other"
